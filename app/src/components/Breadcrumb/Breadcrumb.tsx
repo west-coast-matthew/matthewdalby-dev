9ad4c05a60dc8506a7976008362130caab7135c4
Matthew Dalby <matthew@matthewdalby.dev>
2025-09-02 10:06:37 -0700

    resolve production build issue

--- app/src/components/Breadcrumb/Breadcrumb.tsx
+++ app/src/components/Breadcrumb/Breadcrumb.tsx
@@ -6,22 +6,6 @@ export interface Props {
   path?: string | undefined | null;
 }
 
-function removeFirstToken(inputString: string): string {
-  // Split the string into an array of tokens based on spaces
-  const tokens = inputString.split(" ");
-
-  // If there are no tokens or only one token, return an empty string or the string itself
-  if (tokens.length <= 1) {
-    return ""; // Or return inputString if you prefer to keep single-token strings
-  }
-
-  // Remove the first token by slicing the array from the second element
-  const remainingTokens = tokens.slice(1);
-
-  // Join the remaining tokens back into a string with spaces
-  return remainingTokens.join(" ");
-}
-
 const BreadcrumbPanel: FC<Props> = ({ path }) => {
   if (path === "none") {
     return (
